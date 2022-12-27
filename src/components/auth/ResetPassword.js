fd52d2cf2492856bba83e98e54bfca28f71a68e7
DanielBright <amazingdev0815@gmail.com>
2022-12-27 23:49:09 +0800

    Reset Password and Confirm Reset Password Page

--- src/components/auth/ResetPassword.js
+++ src/components/auth/ResetPassword.js
@@ -1,8 +1,8 @@
+import { ArrowLeftIcon, KeyIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import LeftSide from "../basic/authLeft";
-import {ArrowLeftIcon, KeyIcon} from "@heroicons/react/24/outline"
 
-const ForgotPassword = () => {
+const ResetPassword = () => {
   return (
     <div className="h-screen md:flex font-poppinslight">
       <LeftSide />
@@ -16,23 +16,41 @@ const ForgotPassword = () => {
             </div>
           </div>
           <h1 className="text-gray-800 font-poppinsBold text-center text-3xl mb-3">
-            Forgot Password?
+            Set new password
           </h1>
           <p className="text-base font-normal text-center text-gray-600 mb-8">
-            No worries, we'll send you reset instructions.
+            Your new password must be different to previously used passwords.
           </p>
           <div className="mb-6">
             <label
-              htmlFor="email"
+              htmlFor="password"
               className="block mb-1.5 text-sm font-poppinsMedium text-gray-900 dark:text-white"
             >
-              Email
+              Password
             </label>
             <input
-              type="email"
-              id="email"
+              type="password"
+              id="password"
+              className="bg-gray-50 border mb-1.5 border-gray-300 text-gray-500 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-3.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="•••••••••"
+              required
+            />
+            <span className="text-sm">
+              Must be at least 8 characters.
+            </span>
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block mb-1.5 text-sm font-poppinsMedium text-gray-900 dark:text-white"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="confirm password"
+              id="confirm password"
               className="bg-gray-50 border border-gray-300 text-gray-500 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-3.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter your email"
+              placeholder="•••••••••"
               required
             />
           </div>
@@ -43,10 +61,10 @@ const ForgotPassword = () => {
             Reset Password
           </button>
           <div className="text-center mt-8">
-          <Link to="/login" className="text-sm font-poppinsBold justify-center flex">
-            <ArrowLeftIcon className="w-3 align-middle mr-2"/>
-            <span>Back to log in</span> 
-          </Link>
+            <Link to="/login" className="text-sm font-poppinsBold justify-center flex">
+              <ArrowLeftIcon className="w-3 align-middle mr-2"/>
+              <span>Back to log in</span> 
+            </Link>
           </div>
         </form>
       </div>
@@ -54,4 +72,4 @@ const ForgotPassword = () => {
   );
 };
 
-export default ForgotPassword;
+export default ResetPassword;
---
+++ src/components/auth/confirmReset.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import LeftSide from "../basic/authLeft";
+import {ArrowLeftIcon, CheckCircleIcon, KeyIcon} from "@heroicons/react/24/outline"
+
+const ConfirmReset = () => {
+  return (
+    <div className="h-screen md:flex font-poppinslight">
+      <LeftSide />
+      <div className="flex md:w-1/2 h-full justify-center py-10 items-center bg-white">
+        <form className="bg-white w-1/2">
+          <div className="flex justify-center mb-6">
+            <div className="bg-success-100 rounded-full p-2">
+              <div className="bg-success-200 rounded-full p-2">
+                <CheckCircleIcon className="h-6 w-6 text-success-600"/>
+              </div>
+            </div>
+          </div>
+          <h1 className="text-gray-800 font-poppinsBold text-center text-3xl mb-3">
+            Password reset
+          </h1>
+          <p className="text-base font-normal text-center text-gray-600 mb-8">
+            Your password has been successfully reset. Click below to log in magically.
+          </p>
+          <button
+            type="submit"
+            className="block w-full bg-primary-600 hover:bg-primary-700 mt-6 py-2 rounded-lg text-white font-semibold mb-2"
+          >
+            Back to login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmReset;
