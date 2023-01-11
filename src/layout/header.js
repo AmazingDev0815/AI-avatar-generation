f2c51030a9d3681bba74123e2217cce94d488dfb
DanielBright <amazingdev0815@gmail.com>
2023-01-12 00:24:54 +0800

    Header responsive

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,10 +1,10 @@
-// import { useState } from "react";
-import { BellIcon, Cog8ToothIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { Bars3Icon, BellIcon, Cog8ToothIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, useNavigate } from "react-router-dom";
 // import { Menu } from "@headlessui/react";
 
 const Header = () => {
-  // const [navbarOpen, setNavbarOpen] = useState(false)
+  const [navbarOpen, setNavbarOpen] = useState(false)
   const navigate = useNavigate();
   const login = true;
   const LoginHandle = () => {
@@ -18,10 +18,20 @@ const Header = () => {
   const handleSetting = () => {
     navigate("/setting");
   }
+  
+  const myAvatarClick = () => {
+    console.log('myAvatar')
+    setNavbarOpen(false)
+  }
+
+  const settingClick = () => {
+    setNavbarOpen(false);
+    navigate("/setting")
+  }
 
   return (
     <>
-      <nav className="border-gray-200 px-5 sm:px-10 md:px-20 rounded w-full">
+      <nav className="border-gray-200 px-3 md:px-20 rounded w-full">
         <div className="flex items-center justify-between">
           <div className="relative flex justify-between md:w-auto md:static md:block">
             <div className="flex justify-center items-center">
@@ -41,36 +51,14 @@ const Header = () => {
                 </div>
               )}
             </div>
-
-            {/* <button
-              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            </button> */}
           </div>
-          {/* <div className={
-              "md:flex items-center justify-between w-full md:w-auto" +
-              (navbarOpen ? " flex" : " hidden")
-            }>
-              <div className="flex flex-col justify-center items-center mt-4 border border-gray-100 rounded-lg bg-gray-50 w-full md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-                <a href="#" className="pr-2">
-                  Avatars
-                </a>
-                <a href="#" className="">
-                  Packs
-                </a>
-              </div>
-          </div> */}
           <div
             className={
               "flex items-center justify-between w-auto"
-              // (navbarOpen ? " flex" : " hidden")
             }
             id="mobile menu"
           >
-            <ul className="flex justify-center items-center p-4 border-gray-100 rounded-lg w-full flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-white">
+            <ul className="flex justify-center items-center p-4 border-gray-100 rounded-lg w-full flex-row md:space-x-8 space-x-3 xs:space-x-6 mt-0 text-sm font-medium border-0 bg-white">
               {login ? (
                 <>
                   <li>
@@ -108,8 +96,21 @@ const Header = () => {
                   </button>
                 </li>
               )}
+              <li>            
+                <button
+                  className="cursor-pointer text-xl leading-none py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none"
+                  type="button"
+                  onClick={() => setNavbarOpen(!navbarOpen)}
+                >
+                  {navbarOpen ? <XMarkIcon className="text-xl h-6 w-6 stroke-[2.5px]" /> : <Bars3Icon className="text-xl h-6 w-6 stroke-[2.5px]" />}
+                </button>
+              </li>
             </ul>
           </div>
+        </div>
+        <div className={"flex-col justify-center px-4 font-poppinsSemiBold " + (navbarOpen ? "flex" : "hidden")} id="menu">
+          <div className="py-9 border-b cursor-pointer hover:text-primary-600" onClick={myAvatarClick}>My Avatars</div>
+          <div className="py-9 flex items-center cursor-pointer hover:text-primary-600" onClick={settingClick}><Cog8ToothIcon className="w-5 h-5 stroke-2 mr-2" />Settings</div>
         </div>
       </nav>
     </>
