9bbd8ad80d7b9043d30ca89da973745f442563b1
DanielBright <amazingdev0815@gmail.com>
2023-01-16 19:46:34 +0800

    Success page mobile responsive

--- src/components/pages/success.js
+++ src/components/pages/success.js
@@ -3,10 +3,10 @@ import MainLayout from "../../layout/mainLayout";
 const Success = () => {
   return (
     <MainLayout>
-      <div className="flex flex-1 flex-col justify-center items-center mb-[200px]">
+      <div className="flex flex-1 flex-col justify-center items-center mb-40 p-5">
         <img src={require('../../assets/img/success.gif')} alt="success" className="w-32 h-32"/>
-        <h1 className="font-poppinsBold text-5xl text-gray-900 mt-3 text-center">Just a little more... </h1>
-        <div className="flex flex-col text-base text-gray-600 mt-6 text-center" id="support_text">
+        <h1 className="font-poppinsBold text-4xl md:text-5xl text-gray-900 mt-3 text-center">Just a little more... </h1>
+        <div className="flex flex-col text-sm md:text-base text-gray-600 mt-6 text-center" id="support_text">
           <span>Congratulations! Your profile pictures have been successfully submitted.</span>
           <span>Our AI engine is now creating new avatars. Once they are ready, we will send you an email notification.</span>
           <span>Thank you for choosing our service!</span>
