e2fb307bf5564adcfaadc37923a52a80007ae4bf
DanielBright <amazingdev0815@gmail.com>
2023-01-18 23:35:19 +0800

    For mobile

--- src/components/myAvatars/created.js
+++ src/components/myAvatars/created.js
@@ -10,7 +10,7 @@ const Created = () => {
         Your Created Avatars
       </div>
       <div
-        className="flex flex-col text-sm md:text-base text-gray-600 mt-6 text-center md:w-2/3 px-5 w-full"
+        className="flex flex-col text-sm md:text-base text-gray-600 mt-6 text-center md:w-2/3 px-3 md:px-5 w-full"
         id="support_text"
       >
         Here you can view all of the avatars our AI engine has created for you.
