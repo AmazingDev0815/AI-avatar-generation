8ba25953aceb5f6adf22c2ad4e3d4e3aeb873e34
DanielBright <amazingdev0815@gmail.com>
2023-01-19 03:54:48 +0800

    Image input style and crop ratio

--- src/components/basic/imageGrid.js
+++ src/components/basic/imageGrid.js
@@ -1,11 +1,24 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
 
 // Rendering individual images
-const Image = ({ image, removeImage }) => {
+const Imagetag = ({ image, removeImage }) => {
   const [crop, setCrop] = useState();
+
+  const [disableCrop, setDisableCrop] = useState(false);
+  const [imgWidth, setImgWidth] = useState(176);
+  const [imgHeight, setImgHeight] = useState(176);
+
+  const img = new Image();
+  img.src = image.src;
+  img.onload = function () {
+    setImgWidth(img.naturalWidth);
+    setImgHeight(img.naturalHeight);
+    setDisableCrop(imgWidth === imgHeight);
+  };
+
   const handleRemove = () => {
     removeImage(image);
   };
@@ -15,18 +28,26 @@ const Image = ({ image, removeImage }) => {
         className="absolute bg-primary-600 text-white p-1 z-10 rounded top-2.5 right-2.5"
         onClick={handleRemove}
       >
-        <XMarkIcon width="12" height="12"/>
+        <XMarkIcon width="12" height="12" />
       </button>
-      <ReactCrop
-        crop={crop}
-        onChange={setCrop}
-      >
-        <img
-          alt={`img - ${image.id}`}
-          src={image.src}
-          className="file-img w-32 h-32 md:w-40 md:h-40 lg:w-[236px] lg:h-[236px] rounded-xl"
-        />
-      </ReactCrop>
+      <div className="w-32 h-32 md:w-40 md:h-40 lg:w-[236px] lg:h-[236px] flex justify-center items-center">
+        <ReactCrop
+          crop={crop}
+          onChange={setCrop}
+          aspect={1}
+          locked={disableCrop}
+        >
+          <img
+            alt={`img - ${image.id}`}
+            src={image.src}
+            className={`file-img rounded-xl ${
+              imgHeight > imgWidth
+                ? "w-auto h-32 md:w-auto md:h-40 lg:w-auto lg:h-[236px]"
+                : "w-32 h-auto md:w-40 md:h-auto lg:w-[236px] lg:h-auto"
+            }`}
+          />
+        </ReactCrop>
+      </div>
     </div>
   );
 };
@@ -39,7 +60,7 @@ const ImageGrid = ({ images, remove }) => {
       remove(file);
     };
     return (
-      <Image image={image} key={`${image.id}-image`} removeImage={index} />
+      <Imagetag image={image} key={`${image.id}-image`} removeImage={index} />
     );
   };
   // Return the list of files//
