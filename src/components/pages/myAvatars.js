6407728f3d6fc7830c20475e6b0931c213ee9426
DanielBright <amazingdev0815@gmail.com>
2023-02-28 11:47:21 +0800

    Update myAvatars by design change and get state every 10s

--- src/components/pages/myAvatars.js
+++ src/components/pages/myAvatars.js
@@ -2,17 +2,24 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MoonLoader } from "react-spinners";
 import MainLayout from "../../layout/mainLayout";
-import { getImageCollections } from "../../redux/product/product";
+import { getImageCollections, getTaskState } from "../../redux/product/product";
 import Created from "../myAvatars/created";
 import NotCreated from "../myAvatars/notCreated";
-import Waiting from "../myAvatars/waiting";
 
 const MyAvatars = () => {
   const store = useSelector((state) => state.product);
   const dispatch = useDispatch();
   const [created, setCreated] = useState(store.products?.items?.length > 0);
   const [loading, setLoading] = useState(false);
+  const MINUTE_MS = 10000;
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch(getTaskState())
+    }, MINUTE_MS);
+  
+    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+  }, [])
   useEffect(() => {
     dispatch(getImageCollections());
   }, []);
@@ -46,10 +53,8 @@ const MyAvatars = () => {
             />
           </div>
         </div>
-      ) : created ? (
+      ) : (created || loading) ? (
         <Created />
-      ) : loading ? (
-        <Waiting />
       ) : (
         <NotCreated />
       )}
