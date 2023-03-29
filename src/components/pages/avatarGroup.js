82f016a630c2627e499f06052b54b105f4e52375
DanielBright <amazingdev0815@gmail.com>
2023-03-29 17:40:41 +0800

    Add rate and mobile responsive

--- src/components/pages/avatarGroup.js
+++ src/components/pages/avatarGroup.js
@@ -1,6 +1,7 @@
 import {
   ArrowDownTrayIcon,
   ChevronRightIcon,
+  StarIcon,
 } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 import Moment from "react-moment";
@@ -51,14 +52,14 @@ const Group = () => {
               My Avatars
             </Link>
             <ChevronRightIcon className="text-gray-600 w-4 h-4 mr-[18px]" />
-            <span className="font-poppinsBold text-primary-600">
+            <span className="font-poppinsBold text-primary-600 min-w-0 truncate ">
               {store.selected.name}
             </span>
           </div>
 
           <div className="flex justify-between items-center mt-3" id="download">
-            <div className="flex flex-col">
-              <h1 className="font-poppinsSemiBold text-3xl text-gray-900">
+            <div className="flex flex-col min-w-0">
+              <h1 className="font-poppinsSemiBold text-2xl sm:text-3xl text-gray-900 truncate">
                 {store.selected.name}
               </h1>
               <span className="text-xs text-gray-500 mt-3">
@@ -67,13 +68,24 @@ const Group = () => {
                 </Moment>
               </span>
             </div>
-            <a
-              href={`${process.env.REACT_APP_BASE_URL}image-collections/download/${id}`}
-              className="px-4 py-2.5 flex text-white hover:bg-primary-700 bg-primary-600 rounded-lg items-center"
-            >
-              <ArrowDownTrayIcon className="w-5 h-5 stroke-2 mr-2" />
-              <span className="text-sm font-poppinsSemiBold">Download All</span>
-            </a>
+            <div className="flex gap-2 flex-col lg:flex-row">
+              <a
+                href={`${process.env.REACT_APP_BASE_URL}image-collections/download/${id}`}
+                className="px-4 py-2.5 flex text-white hover:bg-primary-700 bg-primary-600 rounded-lg items-center"
+              >
+                <ArrowDownTrayIcon className="w-5 h-5 stroke-2" />
+                <span className="hidden xs:flex text-sm font-poppinsSemiBold ml-2 whitespace-nowrap">Download All</span>
+              </a>
+              <a
+                target="_blank"
+                href="https://jzkats7aqmw.typeform.com/to/loaM8eUY"
+                className="px-4 py-2.5 flex text-white hover:bg-primary-700 bg-primary-600 rounded-lg items-center"
+              >
+                <StarIcon className="w-5 h-5 stroke-2" />
+                <span className="hidden xs:flex text-sm font-poppinsSemiBold ml-2">Rate</span>
+              </a>
+            </div>
+           
           </div>
           {store.selected?.groups.map((group, key) => (
             <div className="mt-16" id={`style_${key}`} key={key}>
